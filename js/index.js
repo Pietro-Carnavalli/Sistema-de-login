03761b697e18a0d5ba71064417927e3862241b65
Pietrin <carnavallip@gmail.com>
2025-08-26 21:49:25 -0300

    Continuacao do projeto

--- js/index.js
+++ js/index.js
@@ -1,19 +1,45 @@
-//Validacao de e-mail//
+//validacao de email//
 function validarEmail() {
+
     let emailDigitado = document.getElementById("email").value;
     let posArroba = emailDigitado.indexOf("@");
     let posPonto = emailDigitado.lastIndexOf(".");
 
     if (posArroba > 0 && posPonto > posArroba + 1 && posPonto < emailDigitado.length - 1) {
         console.log("Resposta armazenada", emailDigitado);
+        return acesso(true);
 
     } else {
         console.log("Email invalido");
+        return acesso(false);
+
     }
 
 }
 
+//Abre senha na tela//
+function acesso(valor) {
+    let campoSenha = document.getElementById("senha");
+
+    if (valor === true) {
+        campoSenha.classList.remove("none");
+    } else {
+        campoSenha.classList.add("none");
+    }
+}
+
 //Validacao de senha//
+function validarSenha() {
+    let senha = document.getElementById('senha').value;
+    if (senha.length >= 8) {
+        return alert("Cadastro concluido")
+    }
+    else {
+        return alert("A senha deve conter 8 caracteres")
+    }
+
+}
+
 
 
 
